6a36cddb62f8875c7828ebee0c3f8ba58d94b961
maarcoagg <maarcoagg@gmail.com>
2019-03-29 19:28:17 -0300

    Create Hello World Exercise.

--- helloworld/src/main/java/br/pucrs/helloworld/api/Helloworld.java
+++ helloworld/src/main/java/br/pucrs/helloworld/api/Helloworld.java
@@ -1,15 +1,14 @@
 package br.pucrs.helloworld.api;
 
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
 
 
 @RestController
 public class Helloworld {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public Mono<String> helloworld() {
         return Mono.just("Hello world");
     
