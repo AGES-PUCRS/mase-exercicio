0e06ef0a7c7c5b30a3b4db8a16779dfaf52b0a67
maarcoagg <maarcoagg@gmail.com>
2019-03-29 20:40:40 -0300

    Atualiza nome da classe 'HelloWorld'

--- helloworld/src/main/java/br/pucrs/helloworld/api/HelloWorld.java
+++ helloworld/src/main/java/br/pucrs/helloworld/api/HelloWorld.java
@@ -1,12 +1,11 @@
 package br.pucrs.helloworld.api;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Mono;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Helloworld {
+public class HelloWorld {
 
     @GetMapping("/hello")
     public Mono<String> helloworld() {
